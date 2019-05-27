0f4402e0beade56f89de94b08c0f637954db81c3
Ed Crewe <edmundcrewe@gmail.com>
2019-05-27 10:25:41 +0100

    add initial main go for testing

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+// ISO country (location) codes.
+type Country struct {
+	gorm.Model
+	Code       string
+	Name       string
+	Latitude   float32
+	Longtitude float32
+	Alias      string
+}
+
+func main() {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	// Migrate the schema
+	db.AutoMigrate(&Country{})
+
+	// Create
+	db.Create(&Country{Code: "AZ", Name: "AZERBAIJAN", Latitude: 40.5, Longtitude: 47.5, Alias: "Azerbaijan"})
+	// Read
+	var country Country
+	db.First(&country, 1)                // find country with id 1
+	db.First(&country, "code = ?", "AZ") // find country with code l1212
+
+	// Update - update country's alias
+	db.Model(&country).Update("Alias", "New Azer")
+
+	// Delete - delete country
+	db.Delete(&country)
+}
