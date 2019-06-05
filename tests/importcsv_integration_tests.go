82f0c4b13dbc99b716aa1ab8ababf3b8f60cc047
Ed Crewe <edmundcrewe@gmail.com>
2019-06-05 19:52:04 +0100

    integration test

--- tests/importcsv_integration_tests.go
+++ tests/importcsv_integration_tests.go
@@ -3,7 +3,7 @@
 package tests
 
 import (
-	"fmt"
+	"database/sql"
 	"github.com/edcrewe/gormcsv/importcsv"
 	"testing"
 )
@@ -18,5 +18,29 @@ func TestMain(m *testing.M) {
 	model := factory.New("country")
 	meta.Setmeta(model, "name,code,latitude,longtitude,alias")
 	m.Run()
+}
+
+func checkCount(rows *sql.Rows) (count int) {
+	for rows.Next() {
+		err:= rows.Scan(&count)
+		if err != nil { return 0}
+	}
+	return count
+}
+/*
+Integration test for importcsv
+Run the import of test models.go to sqlite and check data is in the db
+*/
+func TestImportCSV(t *testing.T) {
+	mcsv := importcsv.ModelCSV{}
+	// Run import
+	mcsv.ImportCSV("tests/fixtures/Country.csv")
+	// Test database is populated
+	db := mcsv.connectDB()
+	rows, err := db.Query("SELECT COUNT(*) as count FROM countries")
+	count := checkCount(rows)
+	if count < 245 {
+		t.Errorf("Total count: %d rows imported. Expected 245", count)
+	}
 }
 
