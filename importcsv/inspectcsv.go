c2424e85d96575a6d7819bfcbe470f9efe35d086
Ed Crewe <edmundcrewe@gmail.com>
2019-06-13 12:12:59 +0100

    add GetField and unit test for it

--- importcsv/inspectcsv.go
+++ importcsv/inspectcsv.go
@@ -1,8 +1,27 @@
 /* Estimate the field types from a CSV file and create a model for it
 Add method to try convert and narrow down to type then write meta
 Need csv read method from ModelCSV.ImportCSV too so maybe refactor OOP for that
+
+uint8       the set of all unsigned  8-bit integers (0 to 255)
+uint16      the set of all unsigned 16-bit integers (0 to 65535)
+uint32      the set of all unsigned 32-bit integers (0 to 4294967295)
+uint64      the set of all unsigned 64-bit integers (0 to 18446744073709551615)
+
+int8        the set of all signed  8-bit integers (-128 to 127)
+int16       the set of all signed 16-bit integers (-32768 to 32767)
+int32       the set of all signed 32-bit integers (-2147483648 to 2147483647)
+int64       the set of all signed 64-bit integers (-9223372036854775808 to 9223372036854775807)
+
+float32     the set of all IEEE-754 32-bit floating-point numbers
+float64     the set of all IEEE-754 64-bit floating-point numbers
+
 */
 package importcsv
+
+import (
+	"regexp"
+	"strings"
+)
 
 type field struct {
 	Name string
@@ -10,9 +29,56 @@ type field struct {
 	Tag string
 }
 
+var mapLength = map[int]string{2:"8", 4:"16", 9:"32", 20:"64"}
+var keysLength = []int {2, 4, 9, 20}
+var reNumber = regexp.MustCompile(`^[-+]?\d*\.?\d*$`)
+
+
 type CSVMeta struct {
 	Meta
 	Models map[string]string
 	Fields map[string][]field
 }
 
+/*
+Take a name and list of values from CSV then test the values to work out the type
+ */
+func (csvmeta *CSVMeta) GetField(name string, valueStrings []string) field{
+	var f = field{Name: name}
+	var typeStr = ""
+	var vLength = 0
+	for _, valueStr := range valueStrings {
+		if typeStr != "string" && reNumber.MatchString(valueStr) {
+			if strings.Index(valueStr, ".") > -1 {
+				typeStr = "float"
+			}
+			if typeStr != "float" {
+				if strings.HasPrefix(valueStr, "-") || strings.HasPrefix(valueStr, "+") {
+					typeStr = "uint"
+				} else if typeStr != "uint" {
+					typeStr = "int"
+				}
+			}
+			if len(valueStr) > vLength {
+				vLength = len(valueStr)
+			}
+		}
+		if typeStr == "" {
+			_, err := csvmeta.Convert(valueStr, "time")
+			if err != nil {
+				typeStr = "string"
+			}
+		}
+	}
+	if typeStr != "string" && typeStr != "time" {
+		for _, length := range keysLength {
+			if vLength <= length {
+				typeStr = typeStr + mapLength[length]
+				break
+			}
+		}
+		if typeStr == "float8" || typeStr == "float16" { typeStr = "float32" }
+	}
+	f.Type = typeStr
+	return f
+}
---
+++ tests/inspectcsv_unit_test.go
@@ -0,0 +1,53 @@
+package tests
+
+
+import (
+	"github.com/edcrewe/gormcsv/importcsv"
+	"strings"
+	"testing"
+)
+
+var csvmeta importcsv.CSVMeta
+/*
+Setup first then run the tests
+*/
+func TestMain(m *testing.M) {
+	csvmeta = importcsv.CSVMeta{}
+	m.Run()
+}
+
+/*
+Test the CSVMeta.GetField function
+*/
+func TestGetField(t *testing.T) {
+
+	type TableTest struct {
+		input   string
+		typeStr string
+		pass    bool
+	}
+
+	var tableTests = []TableTest {
+		{"23,24,55", "int8", true},
+		{"21474836423412345,234567,23423423", "int64", true},
+		{"-123,24,+12", "uint16", true},
+		{"-123,24,201", "int16", false},
+		{"32770,234234", "int16", false},
+		{"214748366,1234562", "int32", true},
+		{"214748364234,234234234233", "int32", false},
+		{"13213.34273,1234.23,-123123", "float32", true},
+		{"2006-01-02T15:04:05", "date", true},
+		{"false", "bool", true},
+	}
+
+	for _, test := range tableTests {
+		input := strings.Split(test.input, ",")
+		output := csvmeta.GetField("test", input)
+		//fmt.Println(fmt.Sprint(output))
+		if (output.Type == test.typeStr && !test.pass || (output.Type != test.typeStr && test.pass)) {
+			t.Errorf("csvmeta.GetField (%s) %s == %v is not %v", test.input, test.typeStr,
+				output.Type, test.pass)
+		}
+	}
+}
+
